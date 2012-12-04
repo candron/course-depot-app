a05ec9b874d33de5f9e72dac67e8c5060b0a6b91
unknown <const@andron1.(none)>
2012-12-04 19:32:20 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseDepotApp::Application.config.secret_token = '1a88433745f882f4c98ee2f7f9d316aad14c2c73f8b3aaaab027e2aacd793782f3727dc9de6b39aaa160c003577494e3c4b071a424d7f0b7ff4656a26703cfc9'
