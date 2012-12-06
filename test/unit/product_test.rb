24d5cd66e40041f110df32d9308f918a3007f65a
Constantin <andronic.constantin@gmail.com>
2012-12-06 20:54:46 +0200

    Added unit test

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -4,4 +4,9 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "validate presence of attrs" do
+  product=Product.new
+  assert product.invalid?
+  assert product.errors[:title].any?
+  end
 end
