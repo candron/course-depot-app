8055c4b6dfa011951cfe4092d46424996dbeda5c
Constantin <andronic.constantin@gmail.com>
2012-12-08 11:48:14 +0200

    Adding assotiations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,16 @@
 class Product < ActiveRecord::Base
+has_many :line_items
+before_destroy :ensure.not_referenced_by_any_line_item
 default_scope :order=> 'price'
 validates :title, :description, :image_url, :price, :presence=>true
 validates :title, :uniqueness=>true
 validates :price, :numericality=>{:greater_than_or_equal_to=>0.01}
 validates :image_url, :format=>{:with=>%r{\.(gif|jpg|png)$}i, :message=>"Must be JPG or PNG or GIF"}
+ def ensure.not_referenced_by_any_line_item
+  if line_items.empty?
+    true
+  else
+    errors.add(:base, 'There are line items')
+    false
+ end	
 end
